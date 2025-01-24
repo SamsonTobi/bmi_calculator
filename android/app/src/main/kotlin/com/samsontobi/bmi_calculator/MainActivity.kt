a2d48ea418195cd566906529f96d9026fe45d0d0
Oluwatobi <samsonadebowale890@gmail.com>
2025-01-24 19:05:30 +0100

    Initial commit, setting themeData

---
+++ android/app/src/main/kotlin/com/samsontobi/bmi_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.samsontobi.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BASIC BMI CALCULATOR',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(child: Text('Body will be here')),
+    );
+  }
+}
